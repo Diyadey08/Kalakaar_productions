9b59c144887f4f2be5a09ed410a381b0c77e5061
Diya <diyadey310804@gmail.com>
2025-08-07 00:22:06 +0530

    fix

--- app/ui-ux/hero-section.tsx
+++ app/ui-ux/hero-section.tsx
@@ -17,7 +17,7 @@ export function HeroSection({ heroImageY }: HeroSectionProps) {
           <h2 className="text-lg md:text-xl font-semibold text-[#246CF4]">
             Advanced Certification in AI-Driven UI/UX Design
           </h2>
-          <h1 className="text-4xl sm:text-3xl md:text-5xl lg:text-6xl xl:text-7xl  font-bold leading-tight text-[#246CF4]">
+          <h1 className="text-3xl sm:text-2xl md:text-4xl lg:text-5xl xl:text-6xl  font-bold leading-tight text-[#246CF4]">
             Be a Certified Design Innovator
           </h1>
           <p className="text-base md:text-lg text-v0-text-light max-w-md">
